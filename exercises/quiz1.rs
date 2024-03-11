d96ec289fd49927ea424b0b7acc0db74bd6c20d8
florentinojink <2742805603@qq.com>
2024-03-11 14:30:49 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+fn calculate_price_of_apples(apple_nums: i32) ->i32 {
+    if apple_nums > 40 {
+        apple_nums
+    } else {
+        apple_nums * 2
+    }
+}
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
